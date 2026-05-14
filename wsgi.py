f8356fda544926036e343a58d51b8989b156d742
Jorge Mora <jorge.mora699@pascualbravo.edu.co>
2026-05-14 17:19:46 -0500

    use flask dev server instead of gunicorn

--- wsgi.py
+++ wsgi.py
@@ -8,4 +8,5 @@ from infrastructure.web.flask_app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
